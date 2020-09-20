f5de1194f2c6cf380accac1b4a262ee9a39267b3
Francisco Daniel Costa <francisco.costa-ext@viavarejo.com.br>
2020-09-20 18:49:44 -0300

    teste

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World - BeanStalk!";	
+		return "Hello World - EC2!";	
 	}
 
 	public static void main(String[] args) {
